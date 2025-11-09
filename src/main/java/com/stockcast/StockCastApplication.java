ee891c55501cb1260982fd0b5a07a90d2804be4a
IDMendis <dinushimendisxp@gmail.com>
2025-11-09 18:01:07 +0530

    Add Spring Boot Real-Time Stock Price Broadcast System project files

---
+++ src/main/java/com/stockcast/StockCastApplication.java
@@ -0,0 +1,11 @@
+package com.stockcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockCastApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StockCastApplication.class, args);
+    }
+}
